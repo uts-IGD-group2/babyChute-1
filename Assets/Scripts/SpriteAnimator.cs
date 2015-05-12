96482f54c8d9fd8656c44c87ba754e86c1107c01
jcochrane@work <johnny@rotorstudios.com>
2015-05-13 01:53:28 +1000

    - WIP: trying to created Random start frames so spawn animations are out of sync

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -4,19 +4,32 @@ using System.Collections;
 public class BirdAnimator : MonoBehaviour {
 
 	public Sprite[] sprites;
-	public float framesPerSecond;
-
+	public float framesPerSecond = 12;
+	private Sprite initSpriteFrame;
 	private SpriteRenderer spriteRenderer;
 
-	// Use this for initialization
-	void Start () {
+	void Awake ()
+	{
+		int rand = Random.Range(0, sprites.Length);
+		print(rand);
+		initSpriteFrame = sprites[rand];
+	}
+
+	void Start () 
+	{
 		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
 		index = index % sprites.Length;
 		spriteRenderer.sprite = sprites[ index ];
+	}
+
+
+	void OnBecameInvisible() 
+	{
+		GetComponent<SpriteRenderer>().sprite = initSpriteFrame;
+		
 	}
 }
