4028b4f6838d2eafcc39b86de7eee90fc90f2d9a
ryan-jesse <ryan.jesse1@gmail.com>
2015-05-24 12:17:38 +1000

    Setting up resources and scenes for splash screen and story

---
+++ Assets/Scripts/UIManagersScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagersScript : MonoBehaviour {
+
+	public void StartGame () {
+		Application.LoadLevel ("BeginStory");
+	}
+
+	public void RestartGame() {
+		Application.LoadLevel ("stage_1");
+	}
+}
