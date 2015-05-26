551697244cdab0d00598e8cf8dd296e643f07da1
Johnny Cochrane <john.cochrane@student.uts.edu.au>
2015-05-26 20:09:00 +1000

    latest

--- Assets/Scripts/UIManagersScript.cs
+++ Assets/Scripts/UIManagersScript.cs
@@ -2,49 +2,56 @@
 using System.Collections;
 
 public class UIManagersScript : MonoBehaviour {
-	public GameObject     startscreen;
-	public SpriteRenderer frame1;
-	public SpriteRenderer frame2;
-	public SpriteRenderer frame3;
+	//private Transform      Startscreen;
+	private SpriteRenderer Story1;
+	private SpriteRenderer Story2;
+	private SpriteRenderer Story3;
 
 
-	void Start(){
-		this.enabled = false;
-		frame1.enabled = false;
-		frame2.enabled = false;
-		frame3.enabled = false;
+
+	void Start()
+	{
+		Story1 = GameObject.Find ("Story1").GetComponent<SpriteRenderer>();
+		Story2 = GameObject.Find ("Story2").GetComponent<SpriteRenderer>();
+		Story3 = GameObject.Find ("Story3").GetComponent<SpriteRenderer>();
+		
+		this.enabled   = true;
+		Story1.enabled = false;
+		Story2.enabled = false;
+		Story3.enabled = false;
 	}
 
-	public void RestartGame() {
-		Application.LoadLevel (2);
+	public void RestartGame() 
+	{
+		Application.LoadLevel (2); // The first plyable scene
 	}
 	
-	public void StartStory() {
-		this.enabled = true;
-		startscreen.GetComponent<Renderer>().enabled = false;
-
-
+	void StartStory () 
+	{
 		Invoke ("loadStory1", 0);
 		Invoke ("loadStory2", 5);
 		Invoke ("loadStory3", 10);
-		Invoke ("StartGame", 15);
+		Invoke ( "startGame", 15);
 	}
 	
 	void loadStory1() {
-		frame1.enabled = true;
+
+		Story1.enabled = false;
+		Story1.enabled = true;
 	}
 	
 	void loadStory2() {
-		frame1.enabled = false;
-		frame2.enabled = true;
+		Story1.enabled = false;
+		Story2.enabled = true;
 	}
 	
 	void loadStory3() {
-		frame2.enabled = false;
-		frame3.enabled = true;
+		Story2.enabled = false;
+		Story3.enabled = true;
 	}
-	
+
 	void StartGame() {
-		Application.LoadLevel (2);
+		Application.LoadLevel(2);
 	}
 }
+
