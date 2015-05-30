2081d4f4e6f3636385eef59c81978c1fc9949d56
Johnny Cochrane <johnny@rotorstudios.com>
2015-05-30 14:51:03 +1000

    - working EndScreen button restart

--- Assets/Scripts/EndScreenController.cs
+++ Assets/Scripts/EndScreenController.cs
@@ -5,32 +5,35 @@ public class EndScreenController : MonoBehaviour {
 
     public GameObject EndStory1;
     public GameObject EndStory2;
-    public GameObject babyObject;
+	public GameObject BabyObject;
+	public GameObject RestartButton;
 	
 	void Start()
 	{
 		this.enabled = true;
         EndStory1.SetActive(false);
         EndStory2.SetActive(false);
-		babyObject.SetActive(false);
+		BabyObject.SetActive(false);
+		RestartButton.SetActive(false);
 
 		Invoke ("loadEndStory1", 0);
-		Invoke ("loadEndStory2", 8);
+		Invoke ("loadEndStory2", 7);
 	}
 
 	void loadEndStory1() {
         EndStory1.SetActive(true);
-        babyObject.SetActive(true);
-		iTween.MoveTo(babyObject, new Vector3 (0, -7, 0), 50 );
+		BabyObject.SetActive(true);
+		iTween.MoveTo(BabyObject, new Vector3 (0, -8, 0), 50 );
 	}
 	
 	void loadEndStory2() {
-        EndStory2.SetActive(true);
+		EndStory2.SetActive(true);
         EndStory1.SetActive(false);
-		babyObject.SetActive(false);
+		BabyObject.SetActive(false);
+		RestartButton.SetActive(true);
 	}
 
-	public void Restart() {
-		Application.LoadLevel(1);
+	public void RestartGame() {
+		Application.LoadLevel(0);
 	}
 }
